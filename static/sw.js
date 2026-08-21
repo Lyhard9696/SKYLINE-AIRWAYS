fe013737429cebaeeaa6dcf6c4ea00fb132d0498
Lyhard9696 <lyhard8787@gmail.com>
2026-08-21 20:00:26 +0200

    Delete static directory

--- static/sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE="skyline-v02";
-const ASSETS=["/","/static/app.css","/static/app.js","/static/icon-192.png","/static/assets/hub_level_1.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
